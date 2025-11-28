6245f5cd1f720210c59194d50d6b0585a19d1149
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2025-11-28 21:23:59 +0500

    file update

--- src/app/components/page.jsx
+++ src/app/components/page.jsx
@@ -8,7 +8,7 @@ const GetAllProduct = () => {
   console.log("Fetching Data", data);
   return (
     <>
-      <h1 className="text-3xl font-bold mb-6 text-center">
+      <h1 className="text-3xl font-bold mb-6 text-center text-green-800">
         Getting All Products
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
